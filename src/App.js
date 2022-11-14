faee12c8f39eca9fad51215cca59cf46d31d7c2c
Ye Min Aung <yeminaung013@gmail.com>
2022-11-14 10:48:31 +0630

    just testing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Kyaw Bon Thar</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React with tailwindcss and deploy React App on github using npm package called gh-pages
         </a>
+        
       </header>
     </div>
   );
